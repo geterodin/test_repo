7b5a3f12a478cb865a4120e5f76b3a55d596145a
My Name <my.email@example.com>
2014-11-25 18:45:16 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FifthApp::Application.config.secret_key_base = '40c5638f93d7ce02c9781c20c06d076620d949f59dedae64f77cf315d5dbdf796a0ffa06d76c08a1218e2696acabbe7700b2a65f8b18cb873a302ca8f4d89d39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FifthApp::Application.config.session_store :cookie_store, key: '_fifth_app_session'
